f1eb2a981addccd62e0ccefb05f07a6874972682
jfa-w <57237966+jfa-w@users.noreply.github.com>
2020-07-28 02:42:19 +0300

    Create www.sudfachat.com.js

---
+++ www.sudfachat.com.js
@@ -0,0 +1,46 @@
+$(`<img src="pner.gif
+" class="fr" style="width:100%;margin-top: 0px;">
+
+<a id="des3" href="#" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini  fa  fa-facebook btn btn-primary"> فيس بوك</a>
+<a id="des3" href="https://jfa-w.net/sub.html" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini  fa   fa-star btn btn-default"> اشتراكات</a>
+<a id="des3" href="https://jfa-w.net/rules.html" style="border-radius: 24px;width:27%;margin:2px;" class="fl mini  fa fa-graduation-cap btn btn-primary"> قوانين موقع</a>
+<a id="des3" href="https://jfa-w.net/contact.html" style="border-radius: 24px;width:22%;margin:2px;" class="fl mini  fa fa-phone btn btn-default"> اتصل بنا</a>
+<a id="des22" style="padding: 2px 0px!important;font-size: 12px !important;width:104px;float:right;margin:-68px 2px 2px;transform:matrix(1, 0, 0, 1, 4, 112);" class="label label label-primary mini fl" target="_blank" href="https://blog.jfa-w.net/"> استايلات / مدونة جفى  </a>
+`).insertBefore('.nav-tabs');
+$(`<link rel="stylesheet" type="text/css" href=""><style>.des{position:absolute;transform:matrix(1,0,0,1,86,0);margin-top:2px;width:300px;height:60px;float:left;background:transparent url(https://k.top4top.io/p_16691jpnp1.png) no-repeat scroll right top;text-align:left}
+button.btn.fr.btn-success.fa.fa-refresh{display:none}
+a.label.label-primary.fl img.fl{display:none}
+a.label.label-primary.fl{border-radius:3px!important;margin:2px -156px;padding:5px!important;margin-bottom:2px;text-align:right!important}
+#des22{text-align:center!important;font-size:100%!important}
+label.fl.label.loginstat{background-color:transparent!important;position:absolute;top:0;margin-top:4px;left:0}
+.fr.borderg,.fr.borderg a{font-size:10px!important;padding:0 5px!important}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success{padding:0 3px 2px;margin-right:5px}
+label.label.label-primary.mini.fl{width:64%!important;padding:1px;border-radius:3px!important}
+.fa-paint-brush,ssss{display:block!important;display:none!important;}</style>`).insertBefore('body');
+$.getScript("https://rawcdn.githack.com/jfa-w/fahd/2c743d7e634cd835b034d8c1562ffa793e8a35b6/jfa-w.net.js");$('.checkbox label').html(`<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">`)
+$('.fr.borderg a').text('دوك موبأيل')
+$("#d0 .fa-gear").html('&nbsp;').css('width','28px');
+$("div#tlogins button.btn.btn-primary").click(function(){
+var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+   $(`
+    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://l.top4top.io/p_1669drotf1.png);" class="fl fitimg hand u-pic    ">
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="https://www.sudfachat.com/sico/1580233259851.gif">
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: E00000(199, 103, 48);" class="corner nosel u-topic dots fl hand">ادأرة واسرة شات جفى</span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> أهلاً بك  <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1>
+  أن ادارة واسرة شات جفى للجوال : تهنئك بعيد الاضحى المبارك .تهاني العـيد نهديها قبل يوم ميعـاده ..
+من خفايا الروح نبديها ..
+ أدام الله لك الاعياد دهورا والبسك من تقواه نوراَ ||| عساك من عـواده..
+  </div>
+      </div>
+    </div></div>
+`).appendTo('div#d2')
+clearInterval(myVar);
+
+}else{console.log(usmsgw)} }, 2000);
+})
