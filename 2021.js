45fba25d1cd6356d3480851e744dd35271a3edc3
jfa-w <57237966+jfa-w@users.noreply.github.com>
2020-12-30 08:01:26 +0300

    Create 2021.js

---
+++ 2021.js
@@ -0,0 +1,17 @@
+try{
+$("<div class='fr borderg' style='padding:2px;background-color:white;float:left;'> استايلات و ارشفة :<a href=''>جفى </a></div>").insertAfter($(".loginstat"));$('.nav-tabs').addClass('fl').css('width','100%');$(`<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" href="https://rawcdn.githack.com/ommaraxx2015/Omar_AL-Haded/18dcbf1f7784dc555d6f4f5ddf53374b7404506c/AL-Haded.css">
+</style>`).insertBefore('body');$(".fa-gear").text("الضبط").css("width","");(document.getElementById("tbox").placeholder="( مـن يلـوم القـلب في حبها )");
+
+$(` <center> <div style =" width: 100٪؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample"> ( شـــات  جفى للـجــوال ) َ</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> `).insertBefore('#d2');$(`<div><marquee direction="right" width="100%" id="AL-Haded0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4"> حييياكم شششات  جفى  للجوال  ... </marquee></div>`).insertBefore('div#l1');$('.u-msg.dots').removeClass('dots');$(".ubnr").remove();$(".dpnl").css('width','310px');$("div#l1 button.btn.btn-primary").bind("click",function(){alert(" أنت من يتغنى لهُ قلبي ويشتاق إلية، فإليك حبي وفرحتي بحلول سنة جديدة وقلوبنا فياضة 
+ بالعشق، دمتي لي حياة يا حياة قلبي وروحي عنيده");})
+if(location.protocol!='https:'){location.protocol='https:';}
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value="" maxlength="10"><i class="fa fa-eye-slash" aria-hidden="true">`)
+$(`<center><div><div width="99.5%" id="" /font/div/div><center>`).insertBefore('#d2');$(`<div style=" color: #FC1717;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', Fontawesome">لُطفاً ♥️ الإعلانات للضروره فقط </div>`).insertBefore('span.fl.fa.fa-send');
+
+
+
+													}
+														catch (e){
+															console.log(e)
+														}
